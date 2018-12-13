d5e066b31f9922b834aa3ee85d3dfc65451f83e8
Xavier Leng <180161T@mymail.nyp.edu.sg>
2018-12-13 14:52:04 +0800

    python code but not working for now

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_login import LoginManager, login_user, login_required, logout_user
+# from flask_login import LoginManager, login_user, login_required, logout_user
 # from.forms import SignupForm
 # from .models import db
 from his import Hist
