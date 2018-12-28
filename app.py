6329c896f0eb513774ab60b65935c9d14836522f
Xavier Leng <180161T@mymail.nyp.edu.sg>
2018-12-29 01:56:04 +0800

    python code but not working for now

--- app.py
+++ app.py
@@ -3,28 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from his import Hist
 
 app = Flask(__name__)
-# app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////database/database.sqlite'
-# login_manager = LoginManager()
-# login_manager.init_app(app)
 Hist = Hist()
-#
-#
-# def init_db():
-#     db.init_app(app)
-#     db.app = app
-#     db.create_all()
-#
-#
-# @login_manager.user_loader
-# def load_user(username):
-#     return User.query.filter_by(username = username).first()
-#
-#
-# @app.route('/protected')
-# @login_required
-# def protected():
-#     return "protected area"
 
 
 @app.route("/")
@@ -34,45 +13,15 @@ def main():
 
 @app.route("/login", methods=('GET', 'POST'))
 def login():
-    # form = SignupForm()
-    # if request.method == 'GET':
-    #     return render_template('Login_Page.html', form=form)
-    # elif request.method == 'POST':
-    #     if form.validate_on_submit():
-    #         user=User.query.filter_by(username=form.username.data).first()
-    #         if user:
-    #             if user.password == form.password.data:
-    #                 login_user(user)
-    #                 return "User logged in"
-    #             else:
-    #                 return "Wrong password"
-    #         else:
-    #             return "Username doesn't exist"
-    # else:
-    #         return "form not validated"
-    return render_template("Login_Page.html", form=login_form)
-
+    return render_template("Login_Page.html")
 
 
 
 
 @app.route("/register", methods=('GET', 'POST'))
 def register():
-    # form = SignupForm()
-    # if request.method == 'GET':
-    #     return render_template('Register.html', form = form)
-    # elif request.method == 'POST':
-    #     if form.validate_on_submit():
-    #         if User.query.filter_by(username=form.username.data).first():
-    #             return "Username already exists"
-    #         else:
-    #             newuser = User(form.username.data, form.password.data)
-    #             db.session.add(newuser)
-    #             db.session.commit()
-    #             return "User created!!!"
-    #     else:
-    #         return "Form didnt validate"
-    return render_template("Register.html", form=form)
+    return render_template("Register.html")
+
 
 
 
@@ -143,7 +92,11 @@ def map():
 @app.route("/logout")
 # @login_required
 def logout():
-    logout_user()
+
     return "Logged out"
+
+
+
+
 if __name__ == "_main_":
     app.run()
