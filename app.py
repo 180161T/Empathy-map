00bfd6258316d5aaafeedf2ea49395806217336b
Xavier Leng <180161T@mymail.nyp.edu.sg>
2019-01-21 23:32:47 +0800

    python code

--- app.py
+++ app.py
@@ -1,23 +1,54 @@
-from flask import Flask, render_template
+from flask import *
+from persistence import *
+import functools
 from his import Hist
 
 app = Flask(__name__)
 Hist = Hist()
 
 
-@app.route("/")
-def main():
-    return render_template("Main Page.html")
-
-
-@app.route("/login", methods=('GET', 'POST'))
+@app.route("/", methods=('GET', 'POST'))
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            user = get_user(username, password)
+            if user is None:
+                error = 'Wrong username and password'
+            else:
+                session['id'] = user.get_id()
+                session['user_name'] = user.get_username()
+                return redirect(url_for('index'))
+        flash(error)
     return render_template("Login_Page.html")
 
 
 @app.route("/register", methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        else:
+            create_user(username, password)
+            return redirect(url_for('Login_Page'))
+        flash(error)
     return render_template("Register.html")
+
+
+@app.route("/")
+def main():
+    return render_template("Main Page.html")
 
 
 @app.route("/remind")
@@ -88,8 +119,9 @@ def map():
 
 @app.route("/logout")
 def logout():
-    return "Logged out"
+    session.clear()
+    return redirect(url_for('Login_Page'))
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=80)
--- persistence.py
+++ persistence.py
@@ -1,1 +1,47 @@
 import shelve
+import uuid
+
+
+class User:
+    def __init__(self):
+        self.id = id
+        self.__username = ''
+        self.__password = ''
+
+    def get_id(self):
+        return self.__id
+
+    def set_username(self, username):
+        self.__username = username
+
+    def set_password(self, password):
+        self.__password = password
+
+    def get_username(self):
+        return self.__username
+
+    def get_password(self):
+        return self.__password
+
+
+loginFile = shelve.open('loginPage')
+
+
+def create_user(username, password):
+    id = str(uuid.uuid4())
+    user = User(id)
+    user.set_username(username)
+    user.set_password(password)
+    loginFile[id] = user
+
+
+def get_user(username, password):
+    klist = list(loginFile.keys())
+    for key in klist:
+        user = loginFile[key]
+        print(user.get_username(), username, user.get_password(), password)
+        if user.get_username() == username and user.get_password() == password:
+            return user
+    return None
+
+
